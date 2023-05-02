72253a95fcbf2c03b5514809ae4daf69eccb334f
Moazzam4321 <Moazzammughal781@gmail.com>
2023-05-02 22:05:48 +0500

    First Commit

---
+++ storage/framework/views/11142946047341edb796d324679e63d9.php
@@ -0,0 +1,5 @@
+<html>
+    <body>
+        <p>Hello,your link for verify urself is <?php echo e($link); ?></p>
+    </body>
+</html><?php /**PATH C:\Users\Moazzam\OneDrive\Desktop\Porjects\Business_managment\resources\views/EmailVerification.blade.php ENDPATH**/ ?>
